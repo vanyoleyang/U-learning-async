81198570a98cfb1aa18e2639f9a246e6a82c480d
vanyole <vanyole@gmail.com>
2018-01-18 18:23:03 +0900

    asdf

---
+++ asyncrl/agent.py
@@ -0,0 +1,233 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import time
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Flatten, Dense, Input, Conv2D
+from keras.models import Model
+import scipy.signal
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+
+class QlearningAgent:
+    def __init__(self, session, action_size, h, w, channels, opt=tf.train.AdamOptimizer(1e-4), gamma=0.99, ae):
+
+        """Creates Q-Learning agent
+        :param session: tensorflow session
+        :param action_size: (int) length of action space
+        :param h: (int) input image height
+        :param w: (int) input image width
+        :param channels: (int) number of image channels
+        :param opt: tensorflow optimizer (by default: Adam optimizer)"""
+        self.gamma = gamma
+        self.action_size = action_size
+        self.opt = opt
+        self.global_step = tf.Variable(0, name='frame', trainable=False)
+        self.frame_inc_op = self.global_step.assign_add(1, use_locking=True)
+        K.set_session(session)
+        self.sess = session
+        with tf.variable_scope('network'):
+            self.action = tf.placeholder('int32', [None], name='action')
+            self.reward_nstep_q = tf.placeholder('float32', [None], name='reward')
+            self.reward_r = tf.placeholder('float32', [None], name='reward_r')
+            q_model, r_model, u_model, self.state, self.q_values, self.r_values, self.u_values \
+                = self._build_model(h, w, channels, ae)
+            self.q_weights = q_model.trainable_weights
+            self.r_weights = r_model.trainable_weights
+            self.u_weights = u_model.trainable_weights
+        with tf.variable_scope('optimizer'):
+            # Zero all actions, except one that was performed
+            action_onehot = tf.one_hot(self.action, self.action_size, 1.0, 0.0)
+            # Predict immediate reward for performed action
+            r_value = tf.reduce_sum(tf.multiply(self.r_values, action_onehot), reduction_indices=1)
+            # predict expected future uncertainty for performed action
+            u_value = tf.reduce_sum(tf.multiply(self.u_values, action_onehot), reduction_indices=1)
+            # Predict expected future reward for performed action
+            q_value = tf.reduce_sum(tf.multiply(self.q_values, action_onehot), reduction_indices=1)
+
+            # Calculate immediate reward errors
+            self.immediate_reward_err = tf.square(self.reward_r - r_value)
+
+            self.u_target = tf.placeholder('float32', self.immediate_reward_err.shape, name='for_u_t')
+
+            self.r_loss = tf.reduce_mean(self.immediate_reward_err)
+            self.u_loss = tf.reduce_mean(tf.square(self.u_target - u_value))
+            self.q_loss = tf.reduce_mean(tf.square(self.reward_nstep_q - q_value))
+            # Define squared mean loss function: (y - y_)^2
+            self.loss = 0.45 * self.q_loss + 0.45 * self.r_loss + 0.1 * self.u_loss
+            # Compute gradients w.r.t. weights
+            grads = tf.gradients(self.loss, tf.trainable_variables())
+            # grads = tf.gradients(self.loss, [self.q_weights, self.r_weights, self.u_weights])
+            # Apply gradient norm clipping
+            grads, _ = tf.clip_by_global_norm(grads, 40.)
+            grads_vars = list(zip(grads, tf.trainable_variables()))
+            self.train_op = opt.apply_gradients(grads_vars)
+        with tf.variable_scope('target_network'):
+            target_m, _, _, self.target_state, self.target_q_values, _, _ = self._build_model(h, w, channels)
+            target_w = target_m.trainable_weights
+        with tf.variable_scope('target_update'):
+            self.target_q_update = [target_w[i].assign(self.q_weights[i])
+                                  for i in range(len(target_w))]
+
+    def discount(self, x, gamma):
+        return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+    @property
+    def frame(self):
+        """:return: global frame"""
+        return self.global_step.eval(session=self.sess)
+
+    def update_target(self):
+        """Synchronizes shared target with local weights"""
+        self.sess.run(self.target_q_update)
+
+    def predict_rewards(self, state):
+        """Predicts reward per action for given state.
+        :param state: array with shape=[batch_size, num_channels, width, height]
+        :type state: nd.array
+        :return: rewards for each action (e.g. [1.2, 5.0, 0.4])
+        :rtype: list"""
+        return self.sess.run(self.q_values, {self.state: state}).flatten()
+
+    def predict_uncertainty(self, state):
+        return self.sess.run(self.u_values, {self.state: state}).flatten()
+
+    def predict_rewards_n_uncertainty(self, state):
+        q_values, u_values = self.sess.run([self.q_values, self.u_values], {self.state: state})
+        return q_values, u_values
+
+    def predict_q_u_r_(self, state):
+        q_values, u_values, r_values = self.sess.run([self.q_values, self.u_values, self.r_values], {self.state: state})
+        return q_values, u_values, r_values
+
+    def get_loss_values(self,state, action, rewards_4q, rewards_4r):
+        for_u_t = self.immediate_reward_err.eval(session=self.sess, feed_dict={
+            self.state: state,
+            self.action: action,
+            self.reward_nstep_q: rewards_4q,
+            self.reward_r: rewards_4r,
+        })
+        for_u_t = self.discount(for_u_t, self.gamma)
+        q_loss, u_loss, r_loss = self.sess.run([self.q_loss, self.u_loss, self.r_loss], feed_dict={
+            self.state: state,
+            self.action: action,
+            self.reward_nstep_q: rewards_4q,
+            self.reward_r: rewards_4r,
+            self.u_target: for_u_t
+        })
+        return q_loss, u_loss, r_loss
+
+    def predict_target(self, state):
+        """Predicts maximum action's reward for given state with target network
+        :param state: array with shape=[batch_size, num_channels, width, height]
+        :type state: nd.array
+        :return: maximum expected reward
+        :rtype: float"""
+        return np.max(self.sess.run(self.target_q_values, {self.target_state: state}).flatten())
+
+    def train(self, states, actions, rewards_4q, rewards_4r):
+        """Trains online network on given states and rewards batch
+        :param states: batch with screens with shape=[N, H, W, C]
+        :param actions: batch with actions indices, e.g. [1, 4, 0, 2]
+        :param rewards: batch with received rewards from given actions (e.g. [0.43, 0.5, -0.1, 1.0])
+        :type states: nd.array
+        :type actions: list
+        :type rewards: list"""
+        for_u_t = self.immediate_reward_err.eval(session=self.sess, feed_dict={
+            self.state: states,
+            self.action: actions,
+            self.reward_nstep_q: rewards_4q,
+            self.reward_r: rewards_4r,
+        })
+        for_u_t = self.discount(for_u_t, self.gamma)
+        self.sess.run(self.train_op, feed_dict={
+            self.state: states,
+            self.action: actions,
+            self.reward_nstep_q: rewards_4q,
+            self.reward_r : rewards_4r,
+            self.u_target : for_u_t
+        })
+
+    def frame_increment(self):
+        """Increments global frame counter"""
+        self.frame_inc_op.eval(session=self.sess)
+
+    def _build_model(self, h, w, channels, fc3_size=256, ae):
+        """Builds DQN model (Mnih et al., 2015)
+        :param h: input layer height
+        :param w: input layer width
+        :param channels: input layer number of channels
+        :param fc3_size: 3rd fully connected layer size (common: 256, 512)"""
+        if ae :
+            print('hello')
+        state = tf.placeholder('float32', shape=(None, h, w, channels), name='state')
+        inputs = Input(shape=(h, w, channels,))
+        shared_model = Conv2D(activation="relu", filters=16, kernel_size=(8, 8), padding="same",
+                       strides=(4, 4), data_format="channels_last")(inputs)
+        shared_model = Conv2D(kernel_size=(4, 4), data_format="channels_last", strides=(2, 2),
+                       filters=32, activation="relu", padding="same")(shared_model)
+        layer_3 = Flatten()(shared_model)
+
+        r_model = Dense(units=fc3_size, activation='relu')(layer_3)
+        r_out = Dense(units=self.action_size, activation='relu')(r_model)
+        r_model = Model(inputs=inputs, outputs=r_out)
+        rvalues = r_model(state)
+
+        u_model = Dense(units=fc3_size, activation='relu')(layer_3)
+        u_out = Dense(units=self.action_size, activation='relu')(u_model)
+        u_model = Model(inputs=inputs, outputs=u_out)
+        uvalues = u_model(state)
+
+        q_model = Dense(units=fc3_size, activation='relu')(layer_3)
+        q_out = Dense(units=self.action_size, activation='linear')(q_model)
+        q_model = Model(inputs=inputs, outputs=q_out)
+        qvalues = q_model(state)
+
+        return q_model, r_model, u_model, state, qvalues, rvalues, uvalues
+
+
+class AgentSummary:
+    """Helper wrapper for summary tensorboard logging"""
+
+    def __init__(self, logdir, agent, env_name):
+        """ :param logdir: path to the log directory
+            :param agent: agent class-wrapper
+            :param env_name: environment name"""
+        with tf.variable_scope('summary'):
+            self.agent = agent
+            self.last_time = time.time()
+            self.last_frames = self.agent.frame
+            scalar_tags = ['fps', 'episode_avg_reward', 'avg_q_value', 'avg_u_value', 'avg_q_loss',
+                    'avg_u_loss', 'avg_r_loss', 'epsilon', 'total_frame_step']
+            self.writer = tf.summary.FileWriter(logdir, self.agent.sess.graph)
+            self.summary_vars = {}
+            self.summary_ph = {}
+            self.summary_ops = {}
+            for k in scalar_tags:
+                self.summary_vars[k] = tf.Variable(0.)
+                self.summary_ph[k] = tf.placeholder('float32', name=k)
+                self.summary_ops[k] = tf.summary.scalar("%s/%s" % (env_name, k), self.summary_vars[k])
+            self.update_ops = []
+            for k in self.summary_vars:
+                self.update_ops.append(self.summary_vars[k].assign(self.summary_ph[k]))
+            self.summary_op = tf.summary.merge(list(self.summary_ops.values()))
+
+    def write_summary(self, tags):
+        """Writes summary to TensorBoard.
+        :param tags: summary dictionary with with keys:
+                     'episode_avg_reward': average episode reward;
+                     'avg_q_value'       : average episode Q-value;
+                     'avg_u_value'       : average episode U-value;
+                     'epsilon'           : current epsilon values;
+                     'total_frame_step'  : current frame step.
+        :type tags: dict"""
+        tags['fps'] = (self.agent.frame - self.last_frames) / (time.time() - self.last_time)
+        self.last_time = time.time()
+        self.last_frames = self.agent.frame
+        self.agent.sess.run(self.update_ops, {self.summary_ph[k]: v for k, v in tags.items()})
+        summary = self.agent.sess.run(self.summary_op,
+                                      {self.summary_vars[k]: v for k, v in tags.items()})
+        self.writer.add_summary(summary, global_step=self.agent.frame)
---
+++ run_dqn.py
@@ -0,0 +1,301 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import random
+import threading as th
+import time
+from datetime import datetime
+
+from six.moves import range  # pylint: disable=redefined-builtin
+import numpy as np
+import tensorflow as tf
+import scipy.signal
+from asyncrl.agent import QlearningAgent, AgentSummary
+from asyncrl.environment import GymWrapperFactory
+
+# To reproduce minimum epsilon sampling
+random.seed(201)
+# Distribution of epsilon exploration chances (0.1 = 0.4; 0.01 = 0.3; 05 = 0.3)
+EPS_MIN_SAMPLES = 4 * [0.1] + 3 * [0.01] + 3 * [0.5]
+
+# Configurations
+tf.app.flags.DEFINE_integer("threads", 8, "Number of threads to use")
+tf.app.flags.DEFINE_boolean("use_cpu", False, "Use CPU or GPU for training (by default is GPU)")
+# Training settings
+tf.app.flags.DEFINE_integer("ae", False, "Total frames (across all threads)")
+tf.app.flags.DEFINE_integer("total_frames", 100000000, "Total frames (across all threads)")
+tf.app.flags.DEFINE_integer("update_interval", 40000, "Update target network after X frames")
+tf.app.flags.DEFINE_float("eps_steps", 4000000.0, "Decrease epsilon over X frames")
+tf.app.flags.DEFINE_float("eps_start", 1.0, "Starting epsilon (initial exploration chance)")
+tf.app.flags.DEFINE_float("gamma", 0.99, "Gamma discount factor")
+tf.app.flags.DEFINE_integer("tmax", 200, "Maximum batch size")
+tf.app.flags.DEFINE_integer("action_repeat", 4, "Applies last action to X next frames")
+tf.app.flags.DEFINE_integer("memory_len", 4, "Memory length - number of stacked input images")
+# Environment settings
+tf.app.flags.DEFINE_string("env", 'Frostbite-v0', "Environment name (available all OpenAI Gym environments)")
+tf.app.flags.DEFINE_boolean("render", False, "Render frames? Significantly slows down training process")
+tf.app.flags.DEFINE_integer("width", 84, "Screen image width")
+tf.app.flags.DEFINE_integer("height", 84, "Screen image height")
+# Logging
+tf.app.flags.DEFINE_integer("test_iter", 100, "Number of test iterations. Used for logging.")
+tf.app.flags.DEFINE_string("logdir", 'logs/Frostbite/logs_uncertainty_0.45r_0.45u_0.1q_no_eps_200step/', "Path to the directory used for checkpoints and loggings")
+tf.app.flags.DEFINE_integer("log_interval", 50000, "Log and checkpoint every X frame")
+# Evaluation
+tf.app.flags.DEFINE_boolean("eval", False, "Disables training, evaluates agent's performance")
+tf.app.flags.DEFINE_string("evaldir", 'eval/', "Path to the evaluation logging")
+tf.app.flags.DEFINE_integer("eval_iter", 5, "Number of evaluation episodes")
+# Optimizer
+tf.app.flags.DEFINE_float("lr", 1e-4, "Starting learning rate")
+FLAGS = tf.app.flags.FLAGS
+# Hide all GPUs for current process if CPU was chosen
+if FLAGS.use_cpu:
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+global_epsilons = [0.] * FLAGS.threads
+training_finished = False
+
+
+def update_epsilon(frames, eps_steps, eps_min):
+    """Anneals epsilon based on current frame
+    :param frames: (float) current frame number
+    :param eps_steps: (int) total number of epsilon annealing steps
+    :param eps_min: (float) minimum allowed epsilon
+    :type frames: float
+    :type eps_steps: int
+    :type eps_min: float"""
+    # eps = FLAGS.eps_start - (frames / eps_steps) * (FLAGS.eps_start - eps_min)
+    return eps_min # eps if eps > eps_min else eps_min
+
+
+def evaluate():
+    """Evaluated current agent, and records a video with it's performance"""
+    envwrap = GymWrapperFactory.make(FLAGS.env,
+                                     actrep=FLAGS.action_repeat,
+                                     memlen=FLAGS.memory_len,
+                                     w=FLAGS.width,
+                                     h=FLAGS.height)
+    with tf.Session() as sess:
+        agent = QlearningAgent(session=sess,
+                               action_size=envwrap.action_size,
+                               h=FLAGS.height,
+                               w=FLAGS.width,
+                               channels=FLAGS.memory_len,
+                               opt=tf.train.AdamOptimizer(FLAGS.lr))
+        sess.run(tf.initialize_all_variables())
+        if not os.path.exists(FLAGS.logdir):
+            print('ERROR! No', FLAGS.logdir, 'folder found!')
+            return
+        ckpt = tf.train.latest_checkpoint(FLAGS.logdir)
+        if ckpt is not None:
+            tf.train.Saver().restore(sess, ckpt)
+            agent.update_target()
+            print('Session was restored from %s' % ckpt)
+        else:
+            print('ERROR! No checkpoint found at', FLAGS.logdir)
+            return
+        envwrap.env.monitor.start(os.path.join(FLAGS.evaldir, FLAGS.env))
+        total_reward = 0
+        for _ in range(FLAGS.eval_iter):
+            s = envwrap.reset()
+            terminal = False
+            while not terminal:
+                reward_per_action = agent.predict_rewards(s)
+                s, r, terminal, info = envwrap.step(np.argmax(reward_per_action), test=True)
+                total_reward += r
+                envwrap.render()
+        envwrap.env.monitor.close()
+        print('Evaluation finished.')
+        print('Average reward per episode: %0.4f' % (total_reward / FLAGS.eval_iter))
+
+
+def test(agent, env, episodes):
+    """Tests agent's performance on given number of games
+    :param agent: agent to test
+    :param env: environment
+    :param episodes: (int) number of testing episodes
+    :type agent: agent.QlearningAgent
+    :type env: environment.GymWrapper"""
+    ep_rewards = []
+    ep_q = []
+    ep_u = []
+    q_loss, u_loss, r_loss = 0., 0., 0.
+    for _ in range(episodes):
+        rewards_4r = []
+        states = []
+        actions = []
+        ep_reward = 0
+        s = env.reset()
+        terminal = False
+        while not terminal:
+            states.append(s)
+            q_values, u_values = agent.predict_rewards_n_uncertainty(s)
+            action = np.argmax(q_values)
+            actions.append(action)
+            s, r, terminal, info = env.step(action, test=True)
+            ep_q.append(np.max(q_values))
+            ep_u.append(u_values[:,action])
+            rewards_4r.append(r)
+            ep_reward += r
+        rewards_4q = discount(rewards_4r, 0.99)
+        q_l, u_l, r_l = agent.get_loss_values(np.vstack(states), actions, rewards_4q, rewards_4r)
+        q_loss += q_l
+        u_loss += u_l
+        r_loss += r_l
+        ep_rewards.append(ep_reward)
+    return ep_rewards, ep_q, ep_u, q_loss/episodes, u_loss/episodes, r_loss/episodes
+
+
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+def train_async_dqn(agent, env, sess, agent_summary, saver, thread_idx=0):
+    """Starts Asynchronous one/n-step Q-Learning.
+    Can be used as a worker for threading.Thread
+    :param agent: learning (asynchronous) agent
+    :param env: environment
+    :param sess: tensorflow session
+    :param agent_summary: object used for summary tensorboard logging
+    :param saver: tensorflow session saver
+    :param thread_idx: (int) thread index. Thread with index=0 used for target network update and logging
+    :type agent: agent.QlearningAgent
+    :type env: environment.GymWrapper
+    :type sess: tensorflow.Session
+    :type agent_summary: agent.AgentSummary
+    :type saver: tensorflow.train.Saver"""
+
+    def discount(x, gamma):
+        return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+    global global_epsilons
+    eps_min = random.choice(EPS_MIN_SAMPLES)
+    epsilon = update_epsilon(agent.frame, FLAGS.eps_steps, eps_min)
+    print('Thread: %d. Sampled min epsilon: %f' % (thread_idx, eps_min))
+    last_logging = agent.frame
+    last_target_update = agent.frame
+    terminal = True
+    # Training loop:
+    while agent.frame < FLAGS.total_frames:
+        batch_states, batch_rewards, batch_actions = [], [], []
+        if terminal:
+            terminal = False
+            screen = env.reset_random()
+        # Batch update loop:
+        while not terminal and len(batch_states) < FLAGS.tmax:
+            # Increment shared frame counter
+            agent.frame_increment()
+            batch_states.append(screen)
+            # Exploration vs Exploitation, E-greedy action choose
+            if random.random() < epsilon:
+                action_index = random.randrange(agent.action_size)
+            else:
+                q_values, u_values = agent.predict_rewards_n_uncertainty(screen)
+                # Choose an action index with maximum expected reward
+                action_index = np.argmax(q_values + u_values)
+            # Execute an action and receive new state, reward for action
+            screen, reward, terminal, info_ = env.step(action_index)
+            # print(info_)
+            reward = np.clip(reward, -1, 1)
+            # one-step Q-Learning: add discounted expected future reward
+            if len(batch_states) == (FLAGS.tmax-1):
+                if not terminal :
+                    reward += FLAGS.gamma * agent.predict_target(screen)
+            if terminal and FLAGS.env == 'MontezumaRevenge-v0' and info_['ale.lives'] != 5:
+                reward -= 1
+            batch_rewards.append(reward)
+            batch_actions.append(action_index)
+        # Accumulate Rewards for n-step
+        rewards_plus = np.asarray(batch_rewards)
+        batch_discounted_rewards4Q = discount(rewards_plus, FLAGS.gamma)
+        # Apply asynchronous gradient update to shared agent
+        agent.train(np.vstack(batch_states), batch_actions, batch_discounted_rewards4Q, batch_rewards)
+        # Anneal epsilon
+        epsilon = update_epsilon(agent.frame, FLAGS.eps_steps, eps_min)
+        global_epsilons[thread_idx] = epsilon  # Logging
+        # Logging and target network update
+        if thread_idx == 0:
+            if agent.frame - last_target_update >= FLAGS.update_interval:
+                last_target_update = agent.frame
+                agent.update_target()
+            if agent.frame - last_logging >= FLAGS.log_interval and terminal:
+                last_logging = agent.frame
+                saver.save(sess, os.path.join(FLAGS.logdir, "sess.ckpt"), global_step=agent.frame)
+                print('Session saved to %s' % FLAGS.logdir)
+                episode_rewards, episode_q, episode_u, q_loss, u_loss, r_loss = test(agent, env, episodes=FLAGS.test_iter)
+                avg_r = np.mean(episode_rewards)
+                avg_q = np.mean(episode_q)
+                avg_u = np.mean(episode_u)
+                avg_eps = np.mean(global_epsilons)
+                print("%s. Avg.Ep.R: %.4f. Avg.Ep.Q: %.2f. Avg.Ep.U: %.2f. Avg.Eps: %.2f. T: %d" %
+                      (str(datetime.now())[11:19], avg_r, avg_q, avg_u, avg_eps, agent.frame))
+                agent_summary.write_summary({
+                    'total_frame_step': agent.frame,
+                    'episode_avg_reward': avg_r,
+                    'avg_q_value': avg_q,
+                    'avg_u_value': avg_u,
+                    'avg_q_loss': q_loss,
+                    'avg_u_loss': u_loss,
+                    'avg_r_loss': r_loss,
+                    'epsilon': avg_eps
+                })
+    global training_finished
+    training_finished = True
+    print('Thread %d. Training finished. Total frames: %s' % (thread_idx, agent.frame))
+
+
+def run(worker):
+    """Launches worker asynchronously in 'FLAGS.threads' threads
+    :param worker: worker function"""
+    print('Starting %s threads.' % FLAGS.threads)
+    processes = []
+    envs = []
+    for _ in range(FLAGS.threads):
+        env = GymWrapperFactory.make(FLAGS.env,
+                                     actrep=FLAGS.action_repeat,
+                                     memlen=FLAGS.memory_len,
+                                     w=FLAGS.width,
+                                     h=FLAGS.height)
+        envs.append(env)
+
+    with tf.Session() as sess:
+        agent = QlearningAgent(session=sess,
+                               action_size=envs[0].action_size,
+                               h=FLAGS.height,
+                               w=FLAGS.width,
+                               channels=FLAGS.memory_len,
+                               opt=tf.train.AdamOptimizer(FLAGS.lr),
+                               gamma = FLAGS.gamma,
+                               ae = FLAGS.ae)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=2)
+        sess.run(tf.global_variables_initializer())
+        if not os.path.exists(FLAGS.logdir):
+            os.makedirs(FLAGS.logdir)
+        ckpt = tf.train.latest_checkpoint(FLAGS.logdir)
+        if ckpt is not None:
+            saver.restore(sess, ckpt)
+            agent.update_target()
+            print('Restoring session from %s' % ckpt)
+        summary = AgentSummary(FLAGS.logdir, agent, FLAGS.env)
+        for i in range(FLAGS.threads):
+            processes.append(th.Thread(target=worker, args=(agent, envs[i], sess, summary, saver, i,)))
+        for p in processes:
+            p.daemon = True
+            p.start()
+        while not training_finished:
+            if FLAGS.render:
+                for i in range(FLAGS.threads):
+                    envs[i].render()
+            time.sleep(.01)
+        for p in processes:
+            p.join()
+
+
+def main():
+    if FLAGS.eval:
+        evaluate()
+    else:
+        run(train_async_dqn)
+
+
+if __name__ == '__main__':
+    main()
